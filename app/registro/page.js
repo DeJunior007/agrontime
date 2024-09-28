9f660b3b9e543b69b41fa2fbea69e603e4d1dccb
DeJunior007 <deiltonp74@gmail.com>
2024-09-28 19:47:46 -0300

    fix: ajustes e estilização para registro de usuario

--- app/registro/page.js
+++ app/registro/page.js
@@ -1,29 +1,12 @@
 "use client";
+
 import React, { useState } from "react";
-import { z } from "zod";
 import Swal from "sweetalert2";
-import InputMask from "react-input-mask";
 import { criarUsuario } from "../api/usuariosAPI";
 import Link from "next/link";
-
-// Validação de schema com zod
-const usuarioSchema = z.object({
-  nomeCompleto: z.string().min(1, "Nome Completo é obrigatório"),
-  documentoFiscal: z.string().min(1, "CPF é obrigatório"),
-  email: z.string().email("E-mail inválido").min(1, "E-mail é obrigatório"),
-  senha: z
-    .string()
-    .min(8, "Senha deve ter no mínimo 8 caracteres")
-    .regex(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&^_+|~=`{}\[\]:";'<>?,.\/\\-])[A-Za-z\d@$!%*#?&^_+|~=`{}\[\]:";'<>?,.\/\\-]{8,}$/,
-      "Senha deve conter letras maiúsculas e minúsculas, números e caracteres especiais"
-    ),
-  celular: z.string().min(1, "Celular é obrigatório"),
-  genero: z.enum(["masculino", "feminino", "nao_binario"], {
-    required_error: "Gênero é obrigatório",
-  }),
-  dataNascimento: z.string().min(1, "Data de Nascimento é obrigatória"),
-});
+import Input from "../components/Input.js";
+import inputFields from "./forms.json";
+import { validateEmail, validatePassword, validateRequiredField } from '../utils/validate';
 
 const ManterUsuario = () => {
   const [usuario, setUsuario] = useState({
@@ -32,40 +15,94 @@ const ManterUsuario = () => {
     email: "",
     senha: "",
     celular: "",
-    genero: "",
+    genero: "M", // Gênero masculino como padrão
     dataNascimento: "",
   });
 
+  const [errors, setErrors] = useState({});
+
   const handleChange = (e) => {
-    setUsuario({
-      ...usuario,
-      [e.target.name]: e.target.value,
-    });
+    const { name, value } = e.target;
+
+    setUsuario((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+
+    setErrors((prev) => ({
+      ...prev,
+      [name]: "",
+    }));
+  };
+
+  const validateUsuario = () => {
+    const errors = {};
+
+    if (!validateRequiredField(usuario.nomeCompleto)) {
+      errors.nomeCompleto = "Nome Completo é obrigatório";
+    }
+    if (!validateRequiredField(usuario.documentoFiscal)) {
+      errors.documentoFiscal = "CPF é obrigatório";
+    }
+    if (!validateEmail(usuario.email)) {
+      errors.email = "E-mail inválido";
+    }
+    if (!validatePassword(usuario.senha)) {
+      errors.senha = "Senha deve ter no mínimo 8 caracteres";
+    }
+    if (!validateRequiredField(usuario.celular)) {
+      errors.celular = "Celular é obrigatório";
+    }
+    if (!validateRequiredField(usuario.genero)) {
+      errors.genero = "Gênero é obrigatório";
+    }
+    if (!validateRequiredField(usuario.dataNascimento)) {
+      errors.dataNascimento = "Data de Nascimento é obrigatória";
+    }
+
+    return errors;
+  };
+
+  const handleBlur = (name) => {
+    const validationErrors = validateUsuario();
+    setErrors((prev) => ({
+      ...prev,
+      [name]: validationErrors[name] || "",
+    }));
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    const result = usuarioSchema.safeParse(usuario);
-
-    if (!result.success) {
-      const errorMessages = result.error.errors
-        .map((err) => err.message)
-        .join("\n");
-      Swal.fire("Erro de validação", errorMessages, "error");
+    
+    const validationErrors = validateUsuario();
+    
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      Swal.fire("Erro de validação", Object.values(validationErrors).join("\n"), "error");
       return;
     }
-
+  
     try {
       await criarUsuario(usuario);
-      Swal.fire("Sucesso", "Usuário cadastrado com sucesso!", "success");
+      Swal.fire({
+        title: "Sucesso",
+        text: "Usuário cadastrado com sucesso!",
+        icon: "success",
+        showCancelButton: false,
+        confirmButtonColor: "#084739",
+        confirmButtonText: "Ir para Login",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          window.location.href = "/";
+        }
+      });
       setUsuario({
         nomeCompleto: "",
         documentoFiscal: "",
         email: "",
         senha: "",
         celular: "",
-        genero: "",
+        genero: "M", // Resetando gênero para masculino
         dataNascimento: "",
       });
     } catch (error) {
@@ -74,97 +111,32 @@ const ManterUsuario = () => {
   };
 
   return (
-    <section className="flex flex-col items-center min-h-screen p-8">
+    <section className="flex flex-col items-center min-h-screen bg-purple-900 p-8">
       <div className="container mt-40 p-8 bg-white rounded-lg shadow-md">
         <h1 className="text-3xl font-bold text-center mb-6">
           Cadastro de Usuário
         </h1>
+        <p className="text-lg text-gray-700 text-center mb-4">
+          Preencha os campos abaixo para criar uma conta e gerenciar sua fazenda de forma eficiente.
+        </p>
         <form onSubmit={handleSubmit} className="space-y-4">
-          <div className="form-group">
-            <label className="block text-gray-700 font-semibold">
-              Nome Completo
-            </label>
-            <input
-              type="text"
-              name="nomeCompleto"
-              value={usuario.nomeCompleto}
-              onChange={handleChange}
-              className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:ring-2 focus:ring-[#084739] focus:border-transparent"
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label className="block text-gray-700 font-semibold">CPF</label>
-            <InputMask
-              mask="999.999.999-99"
-              name="documentoFiscal"
-              value={usuario.documentoFiscal}
-              onChange={handleChange}
-              className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:ring-2 focus:ring-[#084739] focus:border-transparent"
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label className="block text-gray-700 font-semibold">E-mail</label>
-            <input
-              type="email"
-              name="email"
-              value={usuario.email}
-              onChange={handleChange}
-              className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:ring-2 focus:ring-[#084739] focus:border-transparent"
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label className="block text-gray-700 font-semibold">Senha</label>
-            <input
-              type="password"
-              name="senha"
-              value={usuario.senha}
-              onChange={handleChange}
-              className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:ring-2 focus:ring-[#084739] focus:border-transparent"
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label className="block text-gray-700 font-semibold">Celular</label>
-            <InputMask
-              mask="(99) 99999-9999"
-              name="celular"
-              value={usuario.celular}
-              onChange={handleChange}
-              className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:ring-2 focus:ring-[#084739] focus:border-transparent"
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label className="block text-gray-700 font-semibold">Gênero</label>
-            <select
-              name="genero"
-              value={usuario.genero}
-              onChange={handleChange}
-              className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:ring-2 focus:ring-[#084739] focus:border-transparent"
-              required
-            >
-              <option value="">Selecione...</option>
-              <option value="masculino">Masculino</option>
-              <option value="feminino">Feminino</option>
-              <option value="nao_binario">Não Binário</option>
-            </select>
-          </div>
-          <div className="form-group">
-            <label className="block text-gray-700 font-semibold">
-              Data de Nascimento
-            </label>
-            <input
-              type="date"
-              name="dataNascimento"
-              value={usuario.dataNascimento}
-              onChange={handleChange}
-              className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:ring-2 focus:ring-[#084739] focus:border-transparent"
-              required
-            />
-          </div>
+          {inputFields.map((field, index) => (
+            <div key={index} className="form-group">
+              <Input
+                id={field.id}
+                type={field.type}
+                name={field.name}
+                label={field.label}
+                value={usuario[field.name]}
+                onChange={handleChange}
+                onBlur={() => handleBlur(field.name)}
+                error={errors[field.name] ? true : false}
+                options={field.options}
+                mask={field.mask}
+              />
+              {errors[field.name] && <p className="text-red-500 text-sm">{errors[field.name]}</p>}
+            </div>
+          ))}
           <button
             type="submit"
             className="w-full py-2 mt-4 bg-[#084739] text-white font-semibold rounded-md hover:bg-[#053f33] transition duration-200"
---
+++ app/registro/zod.js
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+const userSchema = z.object({
+  nomeCompleto: z.string().min(1, "Nome Completo é obrigatório"),
+  documentoFiscal: z.string().min(1, "CPF é obrigatório"),
+  email: z.string().email("E-mail inválido"),
+  senha: z.string().min(8, "Senha deve ter no mínimo 8 caracteres"),
+  celular: z.string().min(1, "Celular é obrigatório"),
+  genero: z.string().min(1, "Gênero é obrigatório"),
+  dataNascimento: z.string().min(1, "Data de Nascimento é obrigatória"),
+});
+
+export { userSchema };
