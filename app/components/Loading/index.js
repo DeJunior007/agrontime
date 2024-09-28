08f762a497e02f10aa192ae4cca45e899b622cca
DeJunior007 <deiltonp74@gmail.com>
2024-09-28 19:47:58 -0300

    feat: novo componente para loading

---
+++ app/components/Loading/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center ">
+      <div className="loader">
+        <style jsx>{`
+          .loader {
+            border: 8px solid #f3f3f3;
+            border-top: 8px solid #084739; 
+            border-radius: 50%;
+            width: 60px;
+            height: 60px;
+            animation: spin 1s linear infinite;
+          }
+
+          @keyframes spin {
+            0% { transform: rotate(0deg); }
+            100% { transform: rotate(360deg); }
+          }
+        `}</style>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
