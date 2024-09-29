18fa42910417b99100598af5ff97509fcdb92657
DeJunior007 <deiltonp74@gmail.com>
2024-09-29 16:00:34 -0300

    feat: importacao fontawesome global

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./globals.css"; 
 import Footer from "./components/footer";
 const inter = Inter({ subsets: ["latin"] });
 
